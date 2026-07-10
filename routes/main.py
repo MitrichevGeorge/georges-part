28dbea1ba3197df7d699acf1a631a8dc68d8c11b
MitrichevGeorge <yegorim@mail.ru>
2026-07-10 13:25:30 +0300

    q2

--- routes/main.py
+++ routes/main.py
@@ -5,3 +5,11 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template("index.html")
+
+@main_bp.route('/map')
+def map_view():
+    return render_template("map.html")
+
+@main_bp.route('/streetview')
+def streetview_view():
+    return render_template("streetview.html")
