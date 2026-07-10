121a02303945d6a41ba8989e8dd32679513ae97a
MitrichevGeorge <131480793+MitrichevGeorge@users.noreply.github.com>
2026-07-10 16:33:17 +0300

    new fix

--- routes/main.py
+++ routes/main.py
@@ -223,5 +223,4 @@ def reviews_page():
             'text': text,
             'created_at': '2026-07-10'
         })
-    print(review_data)
     return render_template('reviews.html', reviews=review_data, attractions=ATTRACTIONS)
